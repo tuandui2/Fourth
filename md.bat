42c46cc298c461da96d33860d8be7997a02d30d4
binbin8099 <18810091915@163.com>
2016-09-28 10:05:32 +0800

    delete

--- md.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo 欢迎使用批量文件夹建立脚本QQ953082389
-echo 请根据提示输入。可以按ctrl+C退出。
-
-set /p val=请输入您要建立的文件夹数：
-set /a varshu=%val%-1 
-for /l %%i in (1,1,%val%) do call mkdir.bat
-echo 文件夹创建完成
-pause
